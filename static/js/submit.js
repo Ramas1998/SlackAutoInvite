620b91187e93276c5fc665e5a0633caa93db0ac0
TeAmp0is0N <rohitrathore.imh55@gmail.com>
2020-10-05 09:06:13 +0530

    add procfile and updates other files

---
+++ static/js/submit.js
@@ -0,0 +1,48 @@
+$(document).ready(function()
+{   
+    $("#invite-form").on('submit', function()
+    {   
+        var inviteeEmail = $("#email").val();
+        console.log(inviteeEmail)
+
+        if (!inviteeEmail || !inviteeEmail.length) {
+            alert('Please enter an email');
+            return false;
+        }
+
+        $.ajax({
+          type: "POST",
+          url: "/",
+          headers: {
+            'X-CSRFToken': csrfToken,
+            },
+          data: { email: inviteeEmail},
+          success: function(data) {
+            handleResponse(data);
+          }
+        });
+        return false;
+    });        
+});
+
+function handleResponse(data) {
+    $("#response-text").text('Successful! Check your email (:');
+    if (data.status === 'fail') {
+      $("#response-text").text(getUserFriendlyError(data.error));
+    }
+    $("#response-div").removeClass('hidden');
+}
+
+function getUserFriendlyError(error){
+  if(error === 'already_in_team'){
+    return 'Already signed up! Click on "Sign in" to get started';
+  } else if(error === 'invalid_email'){
+    return 'Thats an invalid email address. Please check again';
+  } else if(error === 'already_invited'){
+    return 'You have been invited already. Check your inbox!';
+  } else if(error === 'invalid_auth'){
+    return 'There seems to be some issues with the setup. Please contact the admin of this channel: codegirls.opensource@gmail.com';
+  } else{
+    return error;
+  }
+}
